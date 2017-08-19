0d2a9b092f6a7e53a6e66dbb5a52b5e8ae0666d0
Tyler Reitz <tdreitz@gmail.com>
2017-08-19 00:31:14 -0700

    exp-hbs finally working

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const burger = require('../models/burger')
+const path = require('path')
 
 const router = express.Router()
 
+const getPath = (url) => path.join(__dirname, '..', '/views/', `${url}.handlebars`)
+
 router.get('/', (req, res) => {
-  res.render('index')
+  burger.get(burgers => {
+    console.log(burgers)
+    res.render(getPath('index'), {burger_data: burgers})
+  })
 })
 
 router.get('/add', (req, res) => {
