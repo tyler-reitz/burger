7fa951794543f400eea180f1dd55bb6494fa5114
Tyler Reitz <tdreitz@gmail.com>
2017-08-28 17:33:11 -0700

    final commit

--- config/orm.js
+++ config/orm.js
@@ -3,25 +3,27 @@ const connection = require('./connection')
 const orm = {
 
   selectAll(tbl, cb) {
+    
     connection.query('SELECT * FROM ??', [tbl], (err, res) => {
       if (err) throw err
       cb(res)
     })
+    
   },
   
-  insertOne(tbl, name, cb) {
-    connection.query('INSERT INTO ?? SET ?', [tbl, {burger_name: name}], (err, res) => {
-      if (err) throw err
-      cb('insert', res)
-    })
-  },
+  // insertOne(tbl, name, cb) {
+  //   connection.query('INSERT INTO ?? SET ?', [tbl, {burger_name: name}], (err, res) => {
+  //     if (err) throw err
+  //     cb('insert', res)
+  //   })
+  // },
   
-  updateOne(tbl, name, cb) {
-    connection.query('UPDATE ?? SET ? WHERE ?', [tbl, {devoured: 1}, {burger_name: name}], (err, res) => {
-      if (err) throw err
-      cb('update', res)
-    })
-  }
+  // updateOne(tbl, name, cb) {
+  //   connection.query('UPDATE ?? SET ? WHERE ?', [tbl, {devoured: 1}, {burger_name: name}], (err, res) => {
+  //     if (err) throw err
+  //     cb('update', res)
+  //   })
+  // }
   
 }
 
